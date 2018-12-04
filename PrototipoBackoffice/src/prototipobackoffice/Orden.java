48535c691fd2c99d2221ee9bfdf9dc3d14c63486
alvaro.montero <alvaro.montero@BLTPO199.blueIT.local>
2018-12-04 12:02:47 +0100

    añadidos atributos clase Orden

--- PrototipoBackoffice/src/prototipobackoffice/Orden.java
+++ PrototipoBackoffice/src/prototipobackoffice/Orden.java
@@ -5,10 +5,25 @@
  */
 package prototipobackoffice;
 
+import java.util.*;
 /**
  *
  * @author alvaro.montero
  */
 public class Orden {
+
+    String BIC_Entidad;
+    String Ref_Orden;
+    String Contrapartida;
+    String BIC_Contrapartida;
+    String Sentido;
+    double Importe;
+    String Divisa;
+    Date Fecha_Valor;
+    String Corresponsal_Propio;
+    String Cuenta_Corresponsal_Propio;
+    String Corresponsal_Ajeno;
+    String Cuenta_Corresponsal_Ajeno;
+    String Tipo_Mensaje;
     
 }
